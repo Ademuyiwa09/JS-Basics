c6ffadf83cb6a445500f3428643aaeea8e09c237
Ademuyiwa <ademuyiwatitilayo59@gmail.com>
2025-01-24 15:29:45 +0100

    Add files via upload

--- Jscript.js
+++ Jscript.js
@@ -2,24 +2,33 @@
 let username;
 document.getElementById("submitBtn").onclick = function () {
   username = document.getElementById("inputName").value;
-  document.getElementById("h1Tag").textContent = `HELLO 🤞   ${username}`
+  document.getElementById("h1Tag").textContent = `HELLO 💪 ${username}`;
 }
 
 // AREA OF A RECTANGLE.
 let length;
 let breadth;
 let area;
+let result;
 document.getElementById("submitButton").onclick = function () {
+  result = document.getElementById("resultParagraph");
   length = document.getElementById("inputLength").value;
   breadth = document.getElementById("inputBreadth").value;
-  area = length * breadth;
-;
+  area = length * breadth;    
+  result.innerHTML = area;
+
   if (area > 0) {
     console.log(`Result: ${area} cm.`);
+    result.style.color = "green"
+    result.innerHTML = `Result: ${area}cm.`;
   } else if (area == 0) {
+    result.style.color = "black";
+    result.innerHTML = `Result: ${area}.`;
     console.log(`Result: ${area}.`);
   } else if(area < 0) {
+    result.style.color = "red"
     console.log(`Negative Result: ${area} cm.`);
+    result.innerHTML = `Negative result: ${area}cm.`;
   }
 
 }
@@ -30,3 +39,4 @@ let evenNumbers = ' ';
 for (evenNumbers = 2; evenNumbers <= 100; evenNumbers+=2) {
   console.log(evenNumbers);
 }
+
